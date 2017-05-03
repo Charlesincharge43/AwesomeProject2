dacd15041fd73c655376a881f9323fba9156384d
Charles <charles.m.long43@gmail.com>
2017-05-02 23:25:35 -0500

    reactnative routes, navbar, login page

--- index.ios.js
+++ index.ios.js
@@ -3,45 +3,117 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import {Scene, Router, Actions } from 'react-native-router-flux';
+// import StatusModal from './components/StatusModal'
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
 export default class AwesomeProject2 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-
-        <Test />
-      </View>
-    );
+      <Router>
+        <Scene key="root">
+          <Scene key="loginPage" component={Login} title="Login" initial={true} />
+          <Scene key="statsPage" component={Stats} navBar={Navbar} title="Stats" />
+          <Scene key="missionPage" component={Missions} navBar={Navbar} title="Missions" />
+          <Scene key="clanPage" component={Clan} navBar={Navbar} title="Clan" />
+          <Scene key="storyPage" component={Story} navBar={Navbar} title="Story" />
+        </Scene>
+      </Router>
+    )
   }
 }
 
-function Test(props){
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to React Native!  asfasdfasdfasdfasdf
-      </Text>
-      <Text style={styles.instructions}>
-        To get started, edit index.ios.js
-      </Text>
-      <Text style={styles.instructions}>
-        Press Cmd+R to reload,{'\n'}
-        Cmd+D or shake for dev menu
-      </Text>
-    </View>
-  )
+class Login extends Component {
+  render(){
+    const gotoStats = () => Actions.statsPage({logininfo: 'blah blah login info'});
+
+    return (
+      // <View style={{margin: 128}}>
+      //   <Text onPress={gotoStats} >Login PlaceHolder (just click here to go to stats/home)</Text>
+      // </View>
+      <View style={styles.container}>
+        <Text>Email:</Text>
+        <TextInput style={styles.input} />
+        <Text>Password:</Text>
+        <TextInput style={styles.input} />
+        <TouchableOpacity>
+          <Text style={styles.button} onPress={gotoStats} >Submit</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+class Navbar extends Component {
+  render(){
+
+    const gotoStats = () => Actions.statsPage({text: 'pass this to stats page!'});
+    const gotoMissions = () => Actions.missionPage({text: 'pass this thingy to missions!'});
+    const gotoClan = () => Actions.clanPage({text: 'pass all tis shiz to clan page!'});
+    const gotoStory = () => Actions.storyPage({text: 'blah blah blahh story page!'});
+
+    return (
+      <View style={styles.navigationBar}>
+        <Text onPress={gotoStats} style={styles.navButton}>Stats</Text>
+        <Text onPress={gotoMissions} style={styles.navButton}>Missions</Text>
+        <Text onPress={gotoClan} style={styles.navButton}>Your Clan</Text>
+        <Text onPress={gotoStory} style={styles.navButton}>Story</Text>
+      </View>)
+  }
+}
+
+
+class Stats extends Component {
+  render() {
+    return (
+      <View style={{margin: 128}}>
+        <Text>Stats Page Placeholder</Text>
+      </View>
+    )
+  }
+}
+
+class Missions extends Component {
+  render() {
+    return (
+      <View style={{margin: 128}}>
+        <Text>Missions Page Placeholder</Text>
+      </View>
+    )
+  }
+}
+
+class Clan extends Component {
+  render() {
+    return (
+      <View style={{margin: 128}}>
+        <Text>Your Clan Page Placeholder</Text>
+      </View>
+    )
+  }
+}
+
+class Story extends Component {
+  render() {
+    return (
+      <View style={{margin: 128}}>
+        <Text>Story Page Placeholder</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -57,6 +129,45 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  navigationBar: {
+    margin: 5,
+    paddingTop: 15,
+    flexDirection: 'row',
+    backgroundColor: 'grey',
+    position: "absolute",
+    paddingTop: 0,
+    bottom: 5,
+    height: 64,
+    right: 0,
+    left: 0,
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'white',
+  },
+  navButton: {
+    color: 'white',
+    textAlign: 'center',
+    margin: 15,
+    width: '17%',
+    // flexBasis: 'auto',
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    height: 30,
+    lineHeight: 30,
+    marginTop: 10,
+    textAlign: 'center',
+    width: 250
+  },
+  input: {
+    margin: 'auto',//CAN'T GET THIS TO CENTER NO MATTER WHAT
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 37,
+    width: 250
+  },
+
 });
 
 AppRegistry.registerComponent('AwesomeProject2', () => AwesomeProject2);
